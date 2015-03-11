5a46782b7b9c1fe485daa0621f02c55b311586ef
dosomeidan <meidan.doso@gmail.com>
2015-03-11 21:49:44 +0200

    כל זיהוי התנועות בערך

--- AnDeTruApp/AnDeTruApp/CameraControl.cs
+++ AnDeTruApp/AnDeTruApp/CameraControl.cs
@@ -21,8 +21,10 @@ namespace AnDeTruApp
         public Texture2D SpriteTexture { get; set; }
         public System.Drawing.Bitmap SpriteBitmap { get; set; }
         public PXCMSenseManager SenseManager { get; set; }
-
+        public PXCMHandModule hand  { get; set; }
+            
         private GraphicsDevice _gd;
+        PXCMHandData handData;
 
         public CameraControl(GraphicsDevice d)
         {
@@ -31,22 +33,84 @@ namespace AnDeTruApp
 
             // Create new manager
             this.SenseManager = PXCMSenseManager.CreateInstance();
-
+            
             // Enable stream
             this.SenseManager.EnableStream(PXCMCapture.StreamType.STREAM_TYPE_COLOR, captureSize.Width, captureSize.Height, 60);
 
+            //Enable Hand
+            SenseManager.EnableHand();//D
+            
             // Create to sprite bitemap for copy
             this.SpriteBitmap = new System.Drawing.Bitmap(captureSize.Width, captureSize.Height);
 
             // set event handler to function
             PXCMSenseManager.Handler handler = new PXCMSenseManager.Handler();
+            hand = SenseManager.QueryHand();
+            handData = hand.CreateOutput();
+            PXCMHandConfiguration handConfiguration = hand.CreateActiveConfiguration();//D
+            handConfiguration.EnableGesture("fist", true);//D
+            handConfiguration.EnableGesture("spreadfingers", true);//D
+            handConfiguration.EnableGesture("v_sign", true);//D
+            handConfiguration.EnableTrackedJoints(true);
+            handConfiguration.SubscribeGesture(OnFiredGesture);
+            handConfiguration.ApplyChanges();
+
+
             handler.onNewSample = OnNewSample;
+            handler.onModuleProcessedFrame = new PXCMSenseManager.Handler.OnModuleProcessedFrameDelegate(onProcessedFrame);
 
             this.SenseManager.Init(handler);
         }
 
+        private void OnFiredGesture(PXCMHandData.GestureData gestureData)
+        {
+            PXCMHandData.JointData[][] nodes = new PXCMHandData.JointData[][] { new PXCMHandData.JointData[0x20], new PXCMHandData.JointData[0x20] };
+            PXCMHandData.IHand IHandData;
+            int numOfHands = handData.QueryNumberOfHands();
+            for (int i = 0; i < numOfHands; i++)
+            {
+                if (handData.QueryHandData(PXCMHandData.AccessOrderType.ACCESS_ORDER_BY_TIME, i, out IHandData) == pxcmStatus.PXCM_STATUS_NO_ERROR)
+                {
+                    if (handData != null)
+                    {
+                        PXCMHandData.JointData jointData;
+                        IHandData.QueryTrackedJoint((PXCMHandData.JointType)1, out jointData);
+                        nodes[i][1] = jointData;
+                    }
+                }
+            }
+            PXCMHandData.GestureData GestureData;//D
+            if (handData.IsGestureFired("fist", out GestureData))//D
+            {
+                //iROCK++;
+            }
+
+            if (handData.IsGestureFired("spreadfingers", out GestureData))//D
+            {
+                //iPaper++;
+            }
+
+            if (handData.IsGestureFired("v_sign", out GestureData))//D
+            {
+                //iScissor++;
+            }
+
+        }
+
+        private pxcmStatus onProcessedFrame(int mid, PXCMBase module, PXCMCapture.Sample sample)
+        {
+            if (mid == PXCMHandModule.CUID)
+            {
+                handData.Update();
+            }
+            
+            // return NO ERROR to continue, or any ERROR to exit the loop
+            return pxcmStatus.PXCM_STATUS_NO_ERROR;
+        }
+
         pxcmStatus OnNewSample(int mid, PXCMCapture.Sample sample)
         {
+            
             // work on sample.color
             PXCMImage.ImageData data;
             pxcmStatus stt = sample.color.AcquireAccess(PXCMImage.Access.ACCESS_READ,
