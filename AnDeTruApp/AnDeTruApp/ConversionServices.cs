22ef58cf8068d66107d342f498cce286b53a0920
kfirstri <kfirstar@gmail.com>
2015-03-11 13:19:09 +0200

    webcam experiments

---
+++ AnDeTruApp/AnDeTruApp/ConversionServices.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.IO;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace AnDeTruApp
+{
+    public static class ConversionServices
+    {
+        public static Texture2D BitmapToTexture2D(GraphicsDevice GraphicsDevice,
+                                                  Bitmap image)
+        {
+            // Buffer size is size of color array multiplied by 4 because   
+            // each pixel has four color bytes  
+            int bufferSize = image.Height * image.Width * 4;
+
+            // Create new memory stream and save image to stream so   
+            // we don't have to save and read file  
+            System.IO.MemoryStream memoryStream =
+                new System.IO.MemoryStream(bufferSize);
+            image.Save(memoryStream, System.Drawing.Imaging.ImageFormat.Png);
+
+            // Creates a texture from IO.Stream - our memory stream  
+            Texture2D texture = Texture2D.FromStream(
+                GraphicsDevice, memoryStream);
+
+            return texture;
+        }
+
+        public static Texture2D BitmapToTexture2D2(GraphicsDevice GraphicsDevice,
+                                                   Bitmap image)
+        {
+            Texture2D tx = null;
+            using (MemoryStream s = new MemoryStream())
+            {
+                image.Save(s, System.Drawing.Imaging.ImageFormat.Png);
+                s.Seek(0, SeekOrigin.Begin); //must do this, or error is thrown in next line
+                tx = Texture2D.FromStream(GraphicsDevice, s);
+            }
+
+            return tx;
+        }
+    }
+}
--- AnDeTruApp/AnDeTruApp/Game1.cs
+++ AnDeTruApp/AnDeTruApp/Game1.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using AForge.Video.DirectShow;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Content;
@@ -8,6 +9,7 @@ using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
+using AForge.Video;
 
 namespace AnDeTruApp
 {
@@ -17,6 +19,8 @@ namespace AnDeTruApp
     public class Game1 : Microsoft.Xna.Framework.Game
     {
         GraphicsDeviceManager graphics;
+        Texture2D spriteTexture;
+        Rectangle spriteRect;
         SpriteBatch spriteBatch;
 
         public Game1()
@@ -48,8 +52,59 @@ namespace AnDeTruApp
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
-            VideoCaptureDevices
+
+            //FilterInfoCollection devices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+
+            //List<string> devicesNames = new List<string>();
+
+            //foreach (FilterInfo device in devices)
+            //{
+            //    devicesNames.Add(device.Name);
+            //}
+
+            //this.spriteRect = new Rectangle();
+
+            VideoCaptureDevice FinalVideo = new VideoCaptureDevice(devices[1].MonikerString);
+            FinalVideo.NewFrame += new NewFrameEventHandler(FinalVideo_NewFrame);
+            FinalVideo.Start();
+
+
+            // Create a SenseManager instance
+            PXCMSenseManager sm = PXCMSenseManager.CreateInstance();
+
+            // Enable depth stream at 320x240x60fps
+            sm.EnableStream(PXCMCapture.StreamType.STREAM_TYPE_DEPTH, 320, 240, 60);
+
+            // Initialize my event handler
+            PXCMSenseManager.Handler handler = new PXCMSenseManager.Handler();
+
+            handler.onNewSample += new PXCMSenseManager.Handler.OnNewSampleDelegate(OnNewSample);
+            sm.Init(handler);
+
+            // Stream depth samples
+            sm.StreamFrames(true);
+
+            // Clean up
+            sm.Dispose();
         }
+
+        pxcmStatus OnNewSample(int mid, PXCMCapture.Sample sample) 
+        {
+           // work on sample.color
+
+           // return NO ERROR to continue, or any ERROR to exit the loop
+           return pxcmStatus.PXCM_STATUS_NO_ERROR;
+        }
+
+
+        //private void FinalVideo_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        //{
+        //    // Convert camera stream to texture
+        //    if (eventArgs.Frame != null)
+        //    {
+        //        this.spriteTexture = ConversionServices.BitmapToTexture2D2(GraphicsDevice, eventArgs.Frame);
+        //    }
+        //}
 
         /// <summary>
         /// UnloadContent will be called once per game and is the place to unload
@@ -85,6 +140,15 @@ namespace AnDeTruApp
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            if (this.spriteTexture != null)
+            {
+                this.spriteRect.Height = GraphicsDevice.PresentationParameters.Bounds.Height;
+                this.spriteRect.Width = GraphicsDevice.PresentationParameters.Bounds.Width;
+
+                spriteBatch.Begin();
+                spriteBatch.Draw(this.spriteTexture, this.spriteRect, Color.White);
+                spriteBatch.End();
+            }
 
             base.Draw(gameTime);
         }
